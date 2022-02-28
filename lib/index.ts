e38fa42e5ac8a17f4b36264f2d9d3348040d7369
Alexandre Barbier <alex@mbpdealexandre.home>
2022-02-28 14:45:54 +0100

    feat(export): add missing export

--- lib/index.ts
+++ lib/index.ts
@@ -1,150 +1,171 @@
-import {Alert} from "./components/Alert"
-import {Background} from "./components/Background"
-import {Bar} from "./components/Bar"
-import {Button, LoadingStatus} from "./components/Button"
-import {Card, CardProps} from "./components/Card"
-import {Checkbox} from "./components/Checkbox"
-import {CircleNotifier} from "./components/CircleNotifier"
-import {CreatableSelect} from "./components/CreatableSelect"
-import {displayPreviewParams, DocumentPreviewModal,} from "./components/DocumentPreviewModal"
-import {FileDownloader} from "./components/FileDownloader"
-import {FilePreviewer} from "./components/FilePreviewer"
-import {FileTrash} from "./components/FileTrash"
+import { Alert } from "./components/Alert";
+import { Background } from "./components/Background";
+import { Bar } from "./components/Bar";
+import { Button, LoadingStatus } from "./components/Button";
+import { Card, CardProps } from "./components/Card";
+import { Checkbox } from "./components/Checkbox";
+import { CircleNotifier } from "./components/CircleNotifier";
+import { CreatableSelect } from "./components/CreatableSelect";
 import {
-    AlignSelfCenter,
-    AlignSelfStart,
-    Column,
-    ColumnCenter,
-    ColumnCenterCenter,
-    ColumnSpacedBetween,
-    ColumnStretch,
-    FlexEnd,
-    FlexShrinkZero,
-    FlexStart,
-    JustifyCenter,
-    Row,
-    RowCenter,
-    SelfAlignedColumn,
-    SpacedBetween,
-    SpacedBetweenCenter,
-    SpacedBetweenEnd,
-    Flex1,
-} from "./components/Flex"
-import {StyledForm} from "./components/Form"
-import {BaseInputProps, Input} from "./components/Input"
-import {InputDate} from "./components/InputDate"
-import {StyledDuoInput} from "./components/InputDuoContainer"
-import {InputMovingPlaceholder} from "./components/InputMovingPlaceholder"
-import {Link} from "./components/Link"
-import {LockableInput} from "./components/LockableInput"
-import {MenuItem} from "./components/MenuItem"
-import {Modal, ModalComponentsProps} from "./components/Modal"
-import {MultiSelect} from "./components/MultiSelect"
-import {NestedMenuItem} from "./components/NestedMenuItem"
-import {PasswordInput} from "./components/PasswordInput"
-import {ProgressBar} from "./components/ProgressBar"
-import {RowCheckbox} from "./components/RowCheckbox"
-import {ScrollableOutlet} from "./components/ScrollableOutlet"
-import {createOptionList, OptionList, Select, Option} from "./components/Select"
-import {Separator, VerticalSeparator} from "./components/Separator"
-import {SortButton} from "./components/SortButton"
-import {Spacer} from "./components/Spacer"
-import {Counter} from "./components/Counter"
-import {SpinningLoader, SpinningLoaderShadow, StyledLoaderShadow,} from "./components/SpinningLoader"
-import {Subtitle} from "./components/Subtitle"
-import {Switch} from "./components/Switch"
-import {Header, Table, TableBuilder, TitleTable} from "./components/Table"
-import {Cell, StyledTitles, TableBody, TableHeader, TableWrapper, WrapperRows,} from "./components/TableStylesForSelect"
-import {TabObject, Tabs} from "./components/Tabs"
-import {Text} from "./components/Text"
-import {Title} from "./components/Title"
-import {StyledToastContainer} from "./components/Toast"
-import { AmountTotals } from "./components/AmountTotals"
-import { CloseCross } from "./components/CloseCross"
+  displayPreviewParams,
+  DocumentPreviewModal,
+} from "./components/DocumentPreviewModal";
+import { FileDownloader } from "./components/FileDownloader";
+import { FilePreviewer } from "./components/FilePreviewer";
+import { FileTrash } from "./components/FileTrash";
+import {
+  AlignSelfCenter,
+  AlignSelfStart,
+  Column,
+  ColumnCenter,
+  ColumnCenterCenter,
+  ColumnSpacedBetween,
+  ColumnStretch,
+  FlexEnd,
+  FlexShrinkZero,
+  FlexStart,
+  JustifyCenter,
+  Row,
+  RowCenter,
+  SelfAlignedColumn,
+  SpacedBetween,
+  SpacedBetweenCenter,
+  SpacedBetweenEnd,
+  Flex1,
+} from "./components/Flex";
+import { StyledForm } from "./components/Form";
+import { BaseInputProps, Input } from "./components/Input";
+import { InputDate } from "./components/InputDate";
+import { StyledDuoInput } from "./components/InputDuoContainer";
+import { InputMovingPlaceholder } from "./components/InputMovingPlaceholder";
+import { Link } from "./components/Link";
+import { LockableInput } from "./components/LockableInput";
+import { MenuItem } from "./components/MenuItem";
+import { Modal, ModalComponentsProps } from "./components/Modal";
+import { MultiSelect } from "./components/MultiSelect";
+import { NestedMenuItem } from "./components/NestedMenuItem";
+import { PasswordInput } from "./components/PasswordInput";
+import { ProgressBar } from "./components/ProgressBar";
+import { RowCheckbox } from "./components/RowCheckbox";
+import { ScrollableOutlet } from "./components/ScrollableOutlet";
+import {
+  createOptionList,
+  OptionList,
+  Select,
+  Option,
+} from "./components/Select";
+import { Separator, VerticalSeparator } from "./components/Separator";
+import { SortButton } from "./components/SortButton";
+import { Spacer } from "./components/Spacer";
+import { Counter } from "./components/Counter";
+import {
+  SpinningLoader,
+  SpinningLoaderShadow,
+  StyledLoaderShadow,
+} from "./components/SpinningLoader";
+import { Subtitle } from "./components/Subtitle";
+import { Switch } from "./components/Switch";
+import { Header, Table, TableBuilder, TitleTable } from "./components/Table";
+import {
+  Cell,
+  StyledTitles,
+  TableBody,
+  TableHeader,
+  TableWrapper,
+  WrapperRows,
+} from "./components/TableStylesForSelect";
+import { TabObject, Tabs } from "./components/Tabs";
+import { Text } from "./components/Text";
+import { Title } from "./components/Title";
+import { StyledToastContainer } from "./components/Toast";
+import { AmountTotals } from "./components/AmountTotals";
+import { CloseCross } from "./components/CloseCross";
+import { InvoiceRecapInfo } from "./components/InvoiceRecap";
 
 export {
-    Alert,
-    Background,
-    Bar,
-    Button,
-    Card,
-    Checkbox,
-    CircleNotifier,
-    CreatableSelect,
-    DocumentPreviewModal,
-    FileDownloader,
-    FilePreviewer,
-    FileTrash,
-    SpacedBetween,
-    SpacedBetweenCenter,
-    SpacedBetweenEnd,
-    Flex1,
-    FlexStart,
-    FlexEnd,
-    FlexShrinkZero,
-    JustifyCenter,
-    Column,
-    ColumnStretch,
-    ColumnSpacedBetween,
-    ColumnCenter,
-    ColumnCenterCenter,
-    Row,
-    RowCenter,
-    AlignSelfStart,
-    AlignSelfCenter,
-    SelfAlignedColumn,
-    StyledForm,
-    Input,
-    InputDate,
-    StyledDuoInput,
-    InputMovingPlaceholder,
-    Link,
-    LockableInput,
-    MenuItem,
-    Modal,
-    MultiSelect,
-    NestedMenuItem,
-    PasswordInput,
-    ProgressBar,
-    RowCheckbox,
-    ScrollableOutlet,
-    Select,
-    createOptionList,
-    Separator,
-    VerticalSeparator,
-    SortButton,
-    Spacer,
-    SpinningLoader,
-    SpinningLoaderShadow,
-    StyledLoaderShadow,
-    Subtitle,
-    Switch,
-    Table,
-    Header,
-    TitleTable,
-    Tabs,
-    Text,
-    Title,
-    StyledToastContainer,
-    TableHeader,
-    TableBody,
-    StyledTitles,
-    TableWrapper,
-    WrapperRows,
-    Cell,
-    Counter,
-    AmountTotals,
-    CloseCross,
-}
+  Alert,
+  Background,
+  Bar,
+  Button,
+  Card,
+  Checkbox,
+  CircleNotifier,
+  CreatableSelect,
+  DocumentPreviewModal,
+  FileDownloader,
+  FilePreviewer,
+  FileTrash,
+  SpacedBetween,
+  SpacedBetweenCenter,
+  SpacedBetweenEnd,
+  Flex1,
+  FlexStart,
+  FlexEnd,
+  FlexShrinkZero,
+  JustifyCenter,
+  Column,
+  ColumnStretch,
+  ColumnSpacedBetween,
+  ColumnCenter,
+  ColumnCenterCenter,
+  Row,
+  RowCenter,
+  AlignSelfStart,
+  AlignSelfCenter,
+  SelfAlignedColumn,
+  StyledForm,
+  Input,
+  InputDate,
+  StyledDuoInput,
+  InputMovingPlaceholder,
+  Link,
+  LockableInput,
+  MenuItem,
+  Modal,
+  MultiSelect,
+  NestedMenuItem,
+  PasswordInput,
+  ProgressBar,
+  RowCheckbox,
+  ScrollableOutlet,
+  Select,
+  createOptionList,
+  Separator,
+  VerticalSeparator,
+  SortButton,
+  Spacer,
+  SpinningLoader,
+  SpinningLoaderShadow,
+  StyledLoaderShadow,
+  Subtitle,
+  Switch,
+  Table,
+  Header,
+  TitleTable,
+  Tabs,
+  Text,
+  Title,
+  StyledToastContainer,
+  TableHeader,
+  TableBody,
+  StyledTitles,
+  TableWrapper,
+  WrapperRows,
+  Cell,
+  Counter,
+  AmountTotals,
+  CloseCross,
+  InvoiceRecapInfo,
+};
 
 export type {
-    CardProps,
-    LoadingStatus,
-    displayPreviewParams,
-    BaseInputProps,
-    ModalComponentsProps,
-    Option,
-    OptionList,
-    TableBuilder,
-    TabObject,
-}
+  CardProps,
+  LoadingStatus,
+  displayPreviewParams,
+  BaseInputProps,
+  ModalComponentsProps,
+  Option,
+  OptionList,
+  TableBuilder,
+  TabObject,
+};
