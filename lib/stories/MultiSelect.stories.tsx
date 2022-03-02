29f585092d4be2e861e41b03d73f3e43f25f6189
Alexandre Barbier <alex@MacBook-Pro-de-Alexandre.local>
2022-03-02 14:47:42 +0100

    feat(storybook): add multiselect example

--- lib/stories/MultiSelect.stories.tsx
+++ lib/stories/MultiSelect.stories.tsx
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 import { Story, Meta } from "@storybook/react";
 
-import { MultiSelect, MultiSelectProps } from "../components/MultiSelect";
+import { MultiSelect } from "../components/MultiSelect";
 import { OptionList } from "../components/Select";
-import { IntlProvider, useIntl } from "react-intl";
+import { useIntl } from "react-intl";
 import { StoryBookWrapper } from "./config/StoryBookWrapper";
 
 export default {
@@ -11,7 +11,7 @@ export default {
   component: MultiSelect,
 } as Meta<typeof MultiSelect>;
 
-const Template: Story<typeof MultiSelect> = (args) => {
+export const SimpleMultiSelectStory: Story<typeof MultiSelect> = (args) => {
   return (
     <StoryBookWrapper>
       <MultiSelectWithIntl />
@@ -64,4 +64,58 @@ const MultiSelectWithIntl = () => {
   );
 };
 
-export const MultiSelectStory = Template.bind({});
+export const MultiSelectWithSelectAllStory: Story<typeof MultiSelect> = (
+  args
+) => {
+  return (
+    <StoryBookWrapper>
+      <MultiSelectWithSelectAll />
+    </StoryBookWrapper>
+  );
+};
+
+const MultiSelectWithSelectAll = () => {
+  const intl = useIntl();
+  const companiesOptions: OptionList<string> = [
+    {
+      value: "google",
+      label: "Google",
+    },
+    {
+      value: "apple",
+      label: "Apple",
+    },
+    {
+      value: "facebook",
+      label: "Facebook",
+    },
+    {
+      value: "amazon",
+      label: "Amazon",
+    },
+    {
+      value: "microsoft",
+      label: "Microsoft",
+    },
+  ];
+
+  const [selectedCompanies, setSelectedCompanies] = useState<readonly any[]>(
+    []
+  );
+
+  return (
+    <MultiSelect
+      intl={intl}
+      field={null}
+      value={selectedCompanies}
+      options={companiesOptions}
+      label={"companies"}
+      domain={"storybook"}
+      optionType={"multiselect"}
+      onChange={(newCompanies) => {
+        setSelectedCompanies(newCompanies);
+      }}
+      canToggleAllOptions={true}
+    />
+  );
+};
